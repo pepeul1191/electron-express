2e7c04bfe849e831d09a9949a04b0d003fe3a1e4
Pepe Valdivia <jvaldivia@softweb.pe>
2020-04-27 13:20:01 -0500

    seteando una ruta de directorio public

--- forms/server.js
+++ forms/server.js
@@ -1,19 +1,38 @@
-const { remote, ipcRenderer } = require('electron')
-const currentWindow = remote.getCurrentWindow()
+const { ipcRenderer } = require('electron')
+const express = require('express')
+const path = require('path')
+const logger = require('morgan')
 
 const txtHost = document.getElementById('txtHost')
 const txtPort = document.getElementById('txtPort')
+const txtDir = document.getElementById('txtDir')
 const btnRun = document.getElementById('btnRun')
+const lbMessage = document.getElementById('lbMessage')
+
+const appExpress = express()
 
 btnRun.addEventListener('click', function(event){
-  event.preventDefault();   // stop the form from submitting
+  // event.preventDefault();   // stop the form from submitting
   let host = txtHost.value
   let port = txtPort.value
+  let dir = txtDir.value
+  let next = true
   if(host == ''){
     host = 'localhost'
   }
   if(port == ''){
     port = '3000'
   }
-  ipcRenderer.send('start-server', host, port)
+  if(dir == ''){
+    next = false
+    lbMessage.innerHTML = 'Debe ingresar una ruta de su disco'
+  }
+  if(next){
+    // 
+    appExpress.use(logger('dev'))
+    appExpress.use(express.static(path.join(dir)))
+    appExpress.listen(port, function () {
+      console.log('Example app listening on port ' + port + '!');
+    });  
+  }
 });
--- main.js
+++ main.js
@@ -1,17 +1,9 @@
 const electron = require('electron')
-const express = require('express')
-const logger = require('morgan')
 const {app, BrowserWindow, ipcMain} = electron
-
-const appExpress = express()
 
 ipcMain.on('start-server', (event, host, port) => {
   console.log(host)
   console.log(port)
-  appExpress.use(logger('dev'))
-  appExpress.listen(port, function () {
-    console.log('Example app listening on port ' + port + '!');
-  });  
 });  
 
 function createWindow () {
